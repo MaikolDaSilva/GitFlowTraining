8cf317667a02b9a52fe807303261a83a800b7c18
MaikolDaSilva <maikol.correia-da-silva@cpnv.ch>
2023-03-09 15:46:45 +0100

    bugfix : end of sub bugfix

--- Calc/MathLib.cs
+++ Calc/MathLib.cs
@@ -9,7 +9,7 @@
 
         public float Sub(float op1, float op2)
         {
-            return op2 - op1;
+            return op1 - op2;
         }
 
         public float Mul(float op1, float op2)
